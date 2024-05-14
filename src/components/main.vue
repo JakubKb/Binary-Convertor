66f5b70064a60965d9c111197e477318bc90d825
JustKooba <jakubsakw2009@gmail.com>
2024-05-14 19:16:35 +0200

    styled the binary input field

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,12 +2,21 @@
   <div class="container">
     <div class="content">
       <h1 class="title">Binary converter</h1>
-      <input type="text" placeholder="Input binary here" />
-      <div class="checbox-label">
-        <p>Numbers</p>
-        <input type="checkbox" />
+      <div class="binary-input-row">
+        <div class="inputs-row">
+          <p>Numbers</p>
+          <input type="checkbox" id="switch" /><label for="switch"
+            >Toggle</label
+          >
+          <p>Letters</p>
+          <input
+            type="text"
+            placeholder="Input binary here"
+            class="binary-input"
+          />
+        </div>
+        <div class="output"></div>
       </div>
-      <div class="output">test</div>
     </div>
   </div>
 </template>
@@ -36,6 +45,8 @@ export default {};
   height: 300px;
   width: 400px;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
 }
 
 .title {
@@ -43,5 +54,71 @@ export default {};
   color: #76abae;
   font-family: "Roboto", sans-serif;
   text-transform: uppercase;
+}
+
+.binary-input {
+  background: transparent;
+  border: #76abae 2px solid;
+  border-radius: 5px;
+  padding: 5px;
+  font-family: "Roboto", sans-serif;
+  margin-right: 5px;
+}
+
+.inputs-row {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: space-around;
+  gap: 5px;
+}
+
+.inputs-row p {
+  text-transform: uppercase;
+  color: #76abae;
+  font-family: "Roboto", sans-serif;
+  font-size: 13px;
+}
+
+input[type="checkbox"] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+
+label {
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 50px;
+  height: 25px;
+  background: grey;
+  display: block;
+  border-radius: 100px;
+  position: relative;
+}
+
+label:after {
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 15px;
+  height: 15px;
+  background: #fff;
+  border-radius: 90px;
+  transition: 0.3s;
+}
+
+input:checked + label {
+  background: #76abae;
+}
+
+input:checked + label:after {
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
+}
+
+label:active:after {
+  width: 10px;
 }
 </style>
